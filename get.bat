3ca0f0e5fb6d9fb38c800363b2bb5c1c5caa9573
ModNope <86792640+ModNope@users.noreply.github.com>
2021-07-01 18:36:33 +0300

    Update get.bat

--- get.bat
+++ get.bat
@@ -1,8 +1,8 @@
-/go mod init Fast
-/go get -u "github.com/hajimehoshi/ebiten"
-/go get -u "github.com/hajimehoshi/ebiten/ebitenutil"
-/go get -u "github.com/hajimehoshi/ebiten/v2/text"
-/go get -u "golang.org/x/image/font"
-//go get -u "golang.org/x/image/font/opentype"
-go get -u "github.com/itchyny/timefmt-go"
+go mod init Fast
+go get -u -v "github.com/hajimehoshi/ebiten"
+go get -u -v "github.com/hajimehoshi/ebiten/ebitenutil"
+go get -u -v "github.com/hajimehoshi/ebiten/v2/text"
+go get -u -v "golang.org/x/image/font"
+go get -u -v "golang.org/x/image/font/opentype"
+go get -u -v "github.com/itchyny/timefmt-go"
 pause
