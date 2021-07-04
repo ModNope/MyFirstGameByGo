da09e7b755d4310e0cc35fb0af593118b4c17e13
ModNope <86792640+ModNope@users.noreply.github.com>
2021-07-04 15:31:21 +0300

    Update build re.bat

--- build re.bat
+++ build re.bat
@@ -1,21 +1,25 @@
 set Name=Game
 set MainFile="./"
+set CMD="cmd"
 set GOOS=windows
 set GOARCH=amd64
 go-winres make --in="./WinDataRes/WindowsDataRes.json" --out="rsrc_%Name%" -arch=%GOARCH%
-go build -o="./cmd/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
+go build -o="./%CMD%/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
+cd %CMD%
+go run "../."
 pause
+cd ../
 set GOOS=windows
 set GOARCH=arm
 go-winres make --in="./WinDataRes/WindowsDataRes.json" --out="rsrc_%Name%" -arch=%GOARCH%
-go build -o="./cmd/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
+go build -o="./%CMD%/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
 set GOOS=windows
 set GOARCH=386
 go-winres make --in="./WinDataRes/WindowsDataRes.json" --out="rsrc_%Name%" -arch=%GOARCH%
-go build -o="./cmd/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
+go build -o="./%CMD%/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
 set GOOS=windows
 set GOARCH=amd64p32
 go-winres make --in="./WinDataRes/WindowsDataRes.json" --out="rsrc_%Name%" -arch=%GOARCH%
-go build -o="./cmd/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
+go build -o="./%CMD%/%Name% %GOOS% %GOARCH%.exe" -ldflags="-H=windowsgui -v" %MainFile%
 pause
 
